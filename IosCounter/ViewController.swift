7a29d3b8583be8900fd198b775d2b6aa2706912d
snowsergo <snowsergo@gmail.com>
2022-07-23 16:10:36 +0500

    Убрал лишние скобки и if  условие, убрал пустые строки

--- IosCounter/ViewController.swift
+++ IosCounter/ViewController.swift
@@ -10,29 +10,20 @@ class ViewController: UIViewController {
     var count: Int = 0
     
     func setLabel() {
-        countLabel.text = "Значение счётчика: \(String(count))"
+        countLabel.text = "Значение счётчика: \(count)"
+        clearButton.isHidden = count == 0
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setLabel()
-        clearButton.isHidden = true
     }
-
     @IBAction func addButtonDidTap(_ sender: Any) {
         count += 1
         setLabel()
-        if (count == 1){
-            clearButton.isHidden = false
-            
-        }
     }
-    
     @IBAction func clearButtonDidTap(_ sender: Any) {
         count = 0
         setLabel()
-        clearButton.isHidden = true
-        
     }
 }
 
